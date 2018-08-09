b28c460d6c6d9cfb6bd19d8155c4a7368fb0736c
ricardcutzh <ricardcutzh@gmail.com>
2018-08-09 15:32:47 -0600

    Avance con la interfaz del horarioa

---
+++ Android/app/src/main/java/com/example/ricar/myapplication/BotonHorario.java
@@ -0,0 +1,13 @@
+package com.example.ricar.myapplication;
+
+import android.content.Context;
+
+import android.widget.Button;
+
+
+public class BotonHorario extends Button {
+
+    public BotonHorario(Context context) {
+        super(context);
+    }
+}
--- Android/app/src/main/java/com/example/ricar/myapplication/Horario.java
+++ Android/app/src/main/java/com/example/ricar/myapplication/Horario.java
@@ -1,7 +1,9 @@
 package com.example.ricar.myapplication;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -25,6 +27,11 @@ public class Horario extends AppCompatActivity {
         Hora h3 = new Hora("08:00");
         Hora h4 = new Hora("09:00");
         Hora h5 = new Hora("10:00");
+        Hora h6 = new Hora("11:00");
+        Hora h7 = new Hora("12:00");
+        Hora h8 = new Hora("13:00");
+        Hora h9 = new Hora("14:00");
+
 
         //LISTA
         ArrayList<Hora> horas = new ArrayList<>();
@@ -32,5 +39,13 @@ public class Horario extends AppCompatActivity {
         horas.add(h3);
         horas.add(h4);
         horas.add(h5);
+        horas.add(h6);
+        horas.add(h7);
+        horas.add(h8);
+        horas.add(h9);
+
+        //ADAPTADOR
+        HorarioListAdapter adapter = new HorarioListAdapter(this, R.layout.adapter_view, horas);
+        visor.setAdapter(adapter);
     }
 }
