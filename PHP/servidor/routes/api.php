b1b9915902d2a1a0ba20e2e7d467bb12305954b3
jorged104 <nowelljorge@gmail.com>
2018-08-11 23:52:26 -0600

    Creacion de Ruta api
    Se creo la ruta de los usuarios para obtener la infomacion del login

--- PHP/servidor/routes/api.php
+++ PHP/servidor/routes/api.php
@@ -16,3 +16,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::middleware('users','UserController',['only'=> ['index']]);
+});
