faf6fbad7ff67bf5e6695c4927d30a3ba7faa8f9
Javier Barreda <javib94usac@github.com>
2018-09-17 20:17:00 -0600

    agregar archivo de actualizacion

---
+++ updateserver.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git pull origin develop 
+sudo rm /var/www/html/servidor
+sudo cp -R /PHP/servidor /var/www/html/
+sudo rm -R /var/www/html/servidor
+sudo cp -R PHP/servidor /var/www/html/
+sudo cp /home/javier_barreda94/.env /var/www/html/servidor/
+cd /var/www/html/servidor
+sudo composer install 
+sudo chown www-data:www-data -R /var/www/html/servidor/
+sudo php artisan make:migration
