5046f98cf64ae44f69276297ad029fe8b169dbd6
Javier Barreda <javib94usac@github.com>
2018-09-19 05:03:52 +0000

    serverupdate

--- updateserver.sh
+++ updateserver.sh
@@ -3,8 +3,6 @@
 git pull origin develop 
 sudo rm /var/www/html/servidor
 sudo cp -R /PHP/servidor /var/www/html/
-sudo rm -R /var/www/html/servidor
-sudo cp -R PHP/servidor /var/www/html/
 sudo cp /home/javier_barreda94/.env /var/www/html/servidor/
 cd /var/www/html/servidor
 sudo composer install 
