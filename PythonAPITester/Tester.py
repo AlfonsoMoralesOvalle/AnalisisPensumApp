e5a881f0a7a8ae0130464c0d9292809255bbbaf9
ricardcutzh <ricardcutzh@gmail.com>
2018-09-19 15:11:36 -0600

    Python Tester para la API, verificar disponibilidad

---
+++ PythonAPITester/Tester.py
@@ -0,0 +1,16 @@
+import unittest
+import APICall
+
+class TestAPI(unittest.TestCase):
+
+    def test_carreras_url(self):
+        result = APICall.testing_carreras_url()
+        self.assertEqual(result, 200)
+
+    def test_registro_url(self):
+        result = APICall.testing_registro_url()
+        self.assertEqual(result, 200)
+
+    def test_login_url(self):
+        result = APICall.testing_login_url()
+        self.assertEqual(result, 200)
