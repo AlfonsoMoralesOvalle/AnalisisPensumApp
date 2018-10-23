dcd010e0432ddae39afb808d6848bb1f243b0477
ChrisLop2017 <crhalfa09@gmail.com>
2018-10-23 17:38:12 -0600

    [ProgramacionParejas] Creado de controlador

---
+++ PHP/servidor/app/Http/Controllers/admin_controller.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Usuario;
+use App\Administrador;
+use Illuminate\Http\Request;
+
+class Admin_controller extends Controller
+{
+
+    public function index(Request $request) {
+
+        if($request->session()->has('usuario_log')) {
+            return view('admin');
+        }else{
+            return view('admin_home');
+        }
+
+    }
+
+}
