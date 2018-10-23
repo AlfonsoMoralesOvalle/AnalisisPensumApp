2402d884db52201722fb0ad8d12f05486424939f
ChrisLop2017 <crhalfa09@gmail.com>
2018-10-23 17:45:13 -0600

    [ProgramacionParejas] Funcion Obtener Usuario

--- PHP/servidor/app/Usuario.php
+++ PHP/servidor/app/Usuario.php
@@ -2,15 +2,25 @@
 
 namespace App;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 
 class Usuario extends Model
 {
-    
-    protected $fillable = 
-    [
-        'user', 
-        'password', 
-        'nombre', 
-    ]; 
+
+    public function get_usuario($datos) {
+
+        $user = DB::table('usuarios')
+            ->where([
+                ['usuarios.user', '=', $datos['user']],
+                ['usuarios.password', '=', $datos['password']],
+            ])
+            ->join('administradors', 'usuarios.idUsuario', '=', 'administradors.idUsuario')
+            ->select('usuarios.*')
+            ->get();
+
+        return $user;
+
+    }
+
 }
