3a5685664179d17ee9001ddc9c94459942373684
Marvin Calderón <2327239740101@ingenieria.usac.edu.gt>
2018-10-23 17:43:08 -0600

    [ProgramacionParejas] Funcion registro usuario

--- PHP/servidor/app/Http/Controllers/admin_controller.php
+++ PHP/servidor/app/Http/Controllers/admin_controller.php
@@ -62,4 +62,72 @@ class Admin_controller extends Controller
 
     }
 
+    public function cerrar_sesion(Request $request) {
+
+
+
+    }
+
+    public function registrar_usuario(Request $request) {
+
+        $code = 0;
+        $msg = "No se realizó ninguna acción.";
+
+        $datos = array(
+            'user'      => $_POST['user'],
+            'password'  => $_POST['pass']
+        );
+
+        $modelo = new Usuario();
+
+        if(count($modelo->get_usuario($datos)) > 0) {
+            $code = 0;
+            $msg = "El usuario ingresado ya existe en la base de datos.";
+        }else{
+
+            $datos['nombre'] = $_POST['nombre'];
+            $datos['estado'] = 1;
+
+            $ins_resp = $modelo->insert_usuario($datos);
+
+            if($ins_resp != 0) {
+
+                $admin_model = new Administrador();
+
+                $inner_datos = array(
+                    'idUsuario' => $ins_resp,
+                    'estado'    => 1
+                );
+
+                $admin_ins_resp = $admin_model->insert_administrador($inner_datos);
+
+                if($admin_ins_resp) {
+
+                    $code = 1;
+                    $msg = "Usuario registrado correctamente.";
+
+                    //Registro la variable de sesión
+                    $request->session()->put('usuario_log', $datos['user']);
+
+                }else{
+                    $code = 2;
+                    $msg = "Usuario creado.  No se pudo hacer administrador.";
+                }
+
+            }else{
+                $code = 0;
+                $msg = "No se pudo registrar el usuario.";
+            }
+
+        }
+
+        $respuesta = array(
+            'code' => $code,
+            'message' => $msg
+        );
+
+        echo json_encode($respuesta);
+
+    }
+
 }
