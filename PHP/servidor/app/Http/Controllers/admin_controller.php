4827f796580880a5ce401e33b7c72166c4cae827
ChrisLop2017 <crhalfa09@gmail.com>
2018-10-23 17:40:43 -0600

    [ProgramacionParejas] Funcion Inicio de Sesion

--- PHP/servidor/app/Http/Controllers/admin_controller.php
+++ PHP/servidor/app/Http/Controllers/admin_controller.php
@@ -29,4 +29,37 @@ class Admin_controller extends Controller
 
     }
 
+    public function inicio_sesion(Request $request) {
+
+        $code = 0;
+        $msg = "No se realizó ninguna acción.";
+
+        $datos = array(
+            'user'      => $_POST['user'],
+            'password'  => $_POST['pass']
+        );
+
+        $modelo = new Usuario();
+
+        if(count($modelo->get_usuario($datos)) > 0) {
+            $code = 1;
+            $msg = "Usuario logueado correctamente";
+
+            //Registro la variable de sesión
+            $request->session()->put('usuario_log', $datos['user']);
+
+        }else{
+            $code = 0;
+            $msg = "Usuario no existe en la base de datos de administradores";
+        }
+
+        $respuesta = array(
+            'code' => $code,
+            'message' => $msg
+        );
+
+        echo json_encode($respuesta);
+
+    }
+
 }
